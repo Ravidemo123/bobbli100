a1550a0ce23f2309ed76e9d87ddf786828ad4afd
Ravidemo123 <raviops100@gmail.com>
2025-08-24 12:55:51 +0530

    feature500 chanes

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -3,6 +3,8 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World from Maven!");
+		// New functionality
+        System.out.println(getFormattedMessage());
     }
     
     public String getMessage() {
